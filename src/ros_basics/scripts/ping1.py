6298af232cb51aeb5a623e69fef10eec4df72b50
AidarIglesias <aidar.iglesias@gmail.com>
2024-10-01 23:35:49 +0200

    ping_msg_fix+comments

--- src/ros_basics/scripts/ping1.py
+++ src/ros_basics/scripts/ping1.py
@@ -3,17 +3,17 @@ import rospy
 from numpy.random import choice as choose
 from std_msgs.msg import String
 
-messages = ["Ping!", "Pong!", "Bleb!", "Meow!"]         # Let's make it fun! There are 4 possible messages chosen randomly
+messages = ["Ping!", "Pong!", "Bleb!", "Meow!"]             # let's make it fun! There are 4 possible messages chosen randomly
 
 def ping():
-    rospy.init_node('ping_node', anonymous=True)            # "ping_node" node 
-    pub = rospy.Publisher('ping', String, queue_size=10)    # "ping" topic 
-    rate = rospy.Rate(1)                                    # Rate of 1Hz (1 msg/sec)
+    rospy.init_node('ping_node', anonymous=True)            # "ping_node" node initialization
+    pub = rospy.Publisher('ping', String, queue_size=10)    # publisher initialization --> publishing to /ping topic
+    rate = rospy.Rate(1)                                    # publishing rate of 1Hz (1 msg/sec)
     while not rospy.is_shutdown():
-        choice = choose(messages, 1)                                # Choose from the string array messages one of the messages (following a uniform distribution)
-        ping_str = "%s! [tstamp: %s]" % (choice, rospy.get_time())  # ping_str is set to the chosen message from before
-        rospy.loginfo(ping_str) 
-        pub.publish(ping_str) 
+        choice = choose(messages, 1)    # choose from the string array messages one of the messages (following a uniform distribution)
+        ping_str = "%s" % choice        # ping_str is set to the chosen message from before
+        rospy.loginfo(ping_str)         # print the message on screen
+        pub.publish(ping_str)           # node /ping_node publishes the message on /ping topic
         rate.sleep()
 
 if __name__ == '__main__':
--- src/ros_basics/scripts/pong1.py
+++ src/ros_basics/scripts/pong1.py
@@ -2,20 +2,21 @@
 import rospy
 from std_msgs.msg import String
 
-pong_str = "Pong!"
-fail_str = "Failed!"
+pong_str = "Pong!"      # string vars definition for matching
+fail_str = "Failed!"    # and non-matching messages
 
 def callback(data):
-    rospy.loginfo("Received %s", data.data) # Print received data on screen
-    if (("%s" % data.data) != "Ping!"):     # If we receive a random message
+    global pong_str, fail_str
+    rospy.loginfo("Received %s", data.data) # print received message on screen
+    if (("%s" % data.data) != "Ping!"):     # if non-matching message is received
         rospy.loginfo("%s" % fail_str)      # send "Failed!"
-    else:                                   # Else
+    else:                                   # else, if matching message (i.e. "Ping!") is received
         rospy.loginfo("%s" % pong_str)      # send "Pong!"
 
 def pong():
-    rospy.init_node('pong_node', anonymous=True)
-    pub = rospy.Publisher('pong', String, queue_size=20)
-    sub = rospy.Subscriber("ping", String, callback)
+    rospy.init_node('pong_node', anonymous=True)            # "pong_node" initialization
+    pub = rospy.Publisher('pong', String, queue_size=20)    # publisher initialization --> publishing to /pong topic
+    sub = rospy.Subscriber("ping", String, callback)        # subscriber initialization --> subscribed to /ping topic
     rospy.spin()
 
 if __name__ == '__main__':
