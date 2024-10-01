565760eb36e4407c18e3272a94af610ceaa582d9
AidarIglesias <aidar.iglesias@gmail.com>
2024-10-01 19:30:20 +0200

    move_l10_to_l6

--- src/ros_basics/scripts/ping1.py
+++ src/ros_basics/scripts/ping1.py
@@ -3,11 +3,12 @@ import rospy
 from numpy.random import choice as choose
 from std_msgs.msg import String
 
+messages = ["Ping!", "Pong!", "Bleb!", "Meow!"]         # Let's make it fun! There are 4 possible messages chosen randomly
+
 def ping():
     rospy.init_node('ping_node', anonymous=True)            # "ping_node" node 
     pub = rospy.Publisher('ping', String, queue_size=10)    # "ping" topic 
     rate = rospy.Rate(1)                                    # Rate of 1Hz (1 msg/sec)
-    messages = ["Ping!", "Pong!", "Bleb!", "Meow!"]         # Let's make it fun! There are 4 possible messages chosen randomly
     while not rospy.is_shutdown():
         choice = choose(messages, 1)                                # Choose from the string array messages one of the messages (following a uniform distribution)
         ping_str = "%s! [tstamp: %s]" % (choice, rospy.get_time())  # ping_str is set to the chosen message from before
