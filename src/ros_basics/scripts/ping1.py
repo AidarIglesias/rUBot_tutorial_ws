cc531c602966222b90bc58b5adc8bab16dc7813b
AidarIglesias <aidar.iglesias@gmail.com>
2024-10-02 00:02:35 +0200

    test_changes_3

--- src/ros_basics/scripts/ping1.py
+++ src/ros_basics/scripts/ping1.py
@@ -6,12 +6,13 @@ from std_msgs.msg import String
 messages = ["Ping!", "Pong!", "Bleb!", "Meow!"]             # let's make it fun! There are 4 possible messages chosen randomly
 
 def ping():
+    global messages ###
     rospy.init_node('ping_node', anonymous=True)            # "ping_node" node initialization
     pub = rospy.Publisher('ping', String, queue_size=100)   # publisher initialization --> publishing to /ping topic
     rate = rospy.Rate(1)                                    # publishing rate of 1Hz (1 msg/sec)
     while not rospy.is_shutdown():
-        choice = choose(messages, 1)    # choose from the string array messages one of the messages (following a uniform distribution)
-        ping_str = "%s" % choice        # ping_str is set to the chosen message from before
+        ### choice = choose(messages, 1)    # choose from the string array messages one of the messages (following a uniform distribution)
+        ping_str = "%s" % (choose(messages, 1)) ### choice        # ping_str is set to the chosen message from before
         rospy.loginfo(ping_str)         # print the message on screen
         pub.publish(ping_str)           # node /ping_node publishes the message on /ping topic
         rate.sleep()
